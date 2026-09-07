cda360b1a2ee1bdbad06c5287d18f66a6e39899d
Zaw-Thet-Paing <zawthetpaing333@gmail.com>
2026-09-07 19:17:34 +0630

    add login page

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<?php
-
-echo "Hello Git";
-
-echo "feature login branch";
-
-// git init
-// git status
-// git add filename
-// git add .
-// git commit -m "message"
-// git log
-// git log --oneline
-
-// working directory -> staging -> repository
+<h1>Login Page</h1>
